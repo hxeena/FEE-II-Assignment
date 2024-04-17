fd83d6c6d2a904eaffa380a6b75199abcec8f98f
hxeena <125298456+hxeena@users.noreply.github.com>
2024-04-17 22:36:25 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import RealEstateProperties from './RealEstateProperties';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Real Estate Properties</h1>
+      <RealEstateProperties />
+    </div>
+  );
+}
+
+export default App;
